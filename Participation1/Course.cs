01c28d923769b031495cbe3f822509ea7f501bd4
Evan Owens <Evan.T.Owens-1@ou.edu>
2019-09-04 11:46:36 -0500

    Add project files.

---
+++ Participation1/Course.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Participation1
+{
+    class Course
+    {
+        public int CourseID { get; set; }
+        public int CourseNumber { get; set; }
+
+        public string CourseName  {get; set; }
+        public string Subject { get; set; }
+
+        public string Instructor;
+
+        public Course()
+        {
+            CourseID = 0;
+            CourseNumber = 0;
+            CourseName = "";
+            Subject = "";
+            Instructor = "";
+        }
+        public override string ToString()
+        {
+            string msg = $"{CourseID}, {Subject}-{CourseNumber}, taught by {Instructor}"; 
+            return msg;
+            
+        }
+
+       
+    }
+
+}
---
+++ Participation1/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Participation1
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int StudentID { get; set; }
+
+        public List<Course> Courses = new List<Course>();
+        
+    Student()
+        {
+            FirstName = "";
+            LastName = "";
+            StudentID = 0;
+        }
+    }
+}
